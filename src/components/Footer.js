454a18688754950971dcfa138d0b7d05dbf58292
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-10-27 22:29:41 +0100

    Finishing up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className='footer  w-100'>
             <hr/>
             <Stack direction="horizontal" gap={3} className='mt-2 mb-2'>
-                <div className="">Zuri.Internship</div>
+                <div className=""><h6>Zuri<em>.</em>Internship</h6></div>
                 <div className="ms-auto">HNG internship 9 - Front End Task</div>
                 <div className="ms-auto"><img src='../I4G-Logo.png' alt='I4G-logo' width='70px' height='15px' /></div>
             </Stack>
