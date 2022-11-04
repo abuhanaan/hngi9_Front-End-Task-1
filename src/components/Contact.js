de30235d1c090c434f52385074629cf96f7f36eb
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-11-04 23:07:53 +0100

    Form ids added as per requirement

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,15 +8,15 @@ import { useFormik } from 'formik'
 export default function Contact() {
     const formik = useFormik({
         initialValues: {
-            firstName: "",
-            lastName: "",
+            first_name: "",
+            last_name: "",
             email: "",
             message: "",
             agree: false
         },
         validationSchema: Yup.object({
-            firstName: Yup.string().max(15, "Has to be 15 Characters or less").required("Required"),
-            lastName: Yup.string().max(15, "Has to be 15 charasters or less").required("Required"),
+            first_name: Yup.string().max(15, "Has to be 15 Characters or less").required("Required"),
+            last_name: Yup.string().max(15, "Has to be 15 charasters or less").required("Required"),
             email: Yup.string().email("Invalid email address").required("Required"),
             message: Yup.string().max(200, "Must be 200 characters or less").required("Required"),
             agree: Yup.bool('Must not be left unchecked')
@@ -38,30 +38,30 @@ export default function Contact() {
                 <div>
                     <Form noValidate validated={formik.validated} onSubmit={formik.handleSubmit}>
                     <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridFirstName">
+                        <Form.Group as={Col} controlId="first_name">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control name='firstName'
-                                          value={formik.values.firstName} 
+                            <Form.Control name='first_name'
+                                          value={formik.values.first_name} 
                                           type="text" 
                                           placeholder="Enter your First Name"
                                           onChange={formik.handleChange}
                                           onBlur={formik.handleBlur} />
-                            {formik.touched.firstName && formik.errors.firstName ? <Alert variant='danger'>{formik.errors.firstName}</Alert> : null}
+                            {formik.touched.first_name && formik.errors.first_name ? <Alert variant='danger'>{formik.errors.first_name}</Alert> : null}
                         </Form.Group>
 
-                        <Form.Group as={Col} controlId="formGridLastName">
+                        <Form.Group as={Col} controlId="last_name">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control name='lastName' 
+                            <Form.Control name='last_name' 
                                           type="text" 
-                                          value={formik.values.lastName}
+                                          value={formik.values.last_name}
                                           placeholder="Enter your Last Name"
                                           onChange={formik.handleChange}
                                           onBlur={formik.handleBlur} />
-                            {formik.touched.lastName && formik.errors.lastName ? <Alert variant='danger'>{formik.errors.lastName}</Alert> : null}
+                            {formik.touched.last_name && formik.errors.last_name ? <Alert variant='danger'>{formik.errors.last_name}</Alert> : null}
                         </Form.Group>
                     </Row>
                     <Row className="mb-3">
-                        <Form.Group controlId="formGridEmail">
+                        <Form.Group controlId="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type='email'
                                           name='email' 
@@ -72,7 +72,7 @@ export default function Contact() {
                             {formik.touched.email && formik.errors.email ? <Alert variant='danger'>{formik.errors.email}</Alert> : null}
                         </Form.Group>
                     </Row>
-                        <Form.Group className="mb-3" controlId="formGridMessage">
+                        <Form.Group className="mb-3" controlId="message">
                             <Form.Label>Message</Form.Label>
                             <Form.Control value={formik.values.message}
                                           as='textarea'
@@ -83,7 +83,7 @@ export default function Contact() {
                                           onBlur={formik.handleBlur} />
                                 {formik.touched.message && formik.errors.message ? <Alert variant='danger'>{formik.errors.message}</Alert> : null}
                             </Form.Group>
-                            <Form.Group className="mb-3" id="formGridCheckbox">
+                            <Form.Group className="mb-3" id="agree">
                                 <Form.Check type="checkbox"
                                             label="You agree to providing your data to {name} who may contact you."
                                             value={formik.values.agree}
@@ -94,7 +94,7 @@ export default function Contact() {
                             </Form.Group>
                     </Form>
 
-                    <Button variant="primary" type="submit" className='w-100'>
+                    <Button id='btn__submit' variant="primary" type="submit" className='w-100'>
                         Submit
                     </Button>
                 </div>
