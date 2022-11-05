7b27fbd37bcc293aa1eaf09989376c4bf9ddb3cf
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-11-05 22:05:14 +0100

    Finishing Up

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stack, Row } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
 
 export default function FooterComponent() {
   return (
@@ -7,13 +7,12 @@ export default function FooterComponent() {
       <footer className="bg-light text-center bottom">
         <div className="text-center" >
             <hr/>
-            <Stack>
+            
             <Row>
                 <div className="col-md-4 ml"><h6>Zuri<em>.</em>Internship</h6></div>
                 <div className="col-md-4">HNG internship 9 - Front End Task</div>
-                <div className="col-md-4 mr"><img src='../I4G-Logo.png' alt='I4G-logo' width='70px' height='15px' /></div>
+                <div className="col-md-4"><img src='../I4G-Logo.png' alt='I4G-logo' width='70px' height='15px' /></div>
             </Row>
-            </Stack>
         </div>
       </footer>
     </>
